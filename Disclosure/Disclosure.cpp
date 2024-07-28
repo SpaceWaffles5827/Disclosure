1b109a9d46bee273f9a1ed9ad53b48100f8e10e8
Jack Hartwig <jackhartwig7@gmail.com>
2024-07-27 23:15:33 -0500

    Add project files.

---
+++ Disclosure/Disclosure.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "config.h"
+
+int main()
+{
+    std::cout << "IP_ADDRESS is: " << IP_ADDRESS << '\n';
+    return 0;
+}
---
+++ Disclosure/config.h
@@ -0,0 +1,6 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define IP_ADDRESS "192.168.1.1"  // Replace with your actual IP address
+
+#endif
